545816bb905ee78440de89f9c3da90fa48ce90bc
veral1533317 <veral1533317@etu.cegepjonquiere.ca>
2018-11-15 16:27:49 -0500

    Exercise joueur est terminer, reste a faire lactivite de validation et cineclub reste onchance a corriger

--- ExercisesJS/Exercice _ Joueurs/JS/traitement.js
+++ ExercisesJS/Exercice _ Joueurs/JS/traitement.js
@@ -1,11 +1,7 @@
-var Cpt,Meilleur,Pire,Recherche;
+var Cpt,Meilleur,Pire;
 Cpt = 0;
 var tabJoueur = new Array(4);
 var tabPoint = new Array(4);
-/*
-document.getElementById("lblReponse").innerHTML = "Voici votre reponse : ";
-Nom = document.getElementById("lblNbreJoueur").value;
-*/
 
 function btnAjouterJoueur_onclick() //fonction ajoute joueur
 {
@@ -23,28 +19,29 @@ function btnAjouterJoueur_onclick() //fonction ajoute joueur
         document.getElementById("btnTrouverPire").disabled = false;
     }
 }
+
 function btnTrouverMoy_onclick()
-{   var moy;
-    moy = calculerMoy();
+{
+    calculerMoy();
 }
+
 function calculerMoy()
-{ var moy,total;
-    moy=0;
+{ var Total,final;
+    final =0;
     Total =0;
-    nomEtu =  0;
     for(var i =0; i < tabPoint.length; i++)
     {
         Total += tabPoint[i];
-        nomEtu = tabJoueur[i];
-        document.getElementById("lblReponse").innerHTML = "La moyenne est de = " + moy + " pour " + nomEtu;
+        final = Total/tabJoueur.length;
+        document.getElementById("lblReponse").innerHTML = "La moyenne est de : " + final;
     }
-    moy = Total/tabJoueur.length;
-    return moy;
 }
+
 function btnTrouverBest_onclick()
 {
     trouverMeilleur();
 }
+
 function trouverMeilleur()
 {
     Meilleur =1;
@@ -55,17 +52,18 @@ function trouverMeilleur()
         {
             Meilleur = tabPoint[i];
             bestEtu = tabJoueur[i];
-            document.getElementById("lblReponse").innerHTML = "Le meilleur score est de  = " + Meilleur + " pour " + bestEtu;
+            document.getElementById("lblReponse").innerHTML = "Le meilleur score est de  : " + Meilleur + " pour " + bestEtu;
         }
     }
     return Meilleur;
 }
+
 function  btnTrouverPire_onclick()
 {
     nomEtu = document.getElementById("txtNom").value;
     trouverPire();
-
 }
+
 function trouverPire()
 {
     Pire = 100;
@@ -76,11 +74,17 @@ function trouverPire()
         {
             Pire = tabPoint[i];
             pireEtu = tabJoueur[i];
-            document.getElementById("lblReponse").innerHTML = "Le pire score est de  = " + Pire + " pour " + pireEtu;
+            document.getElementById("lblReponse").innerHTML = "Le pire score est de : " + Pire + " pour " + pireEtu;
         }
     }
 }
+
 function btnRechercher_onclick()
+{
+    trouverEtu();
+}
+
+function trouverEtu()
 {
     var trouve,i,nomEtu;
     trouve =false;
@@ -101,14 +105,11 @@ function btnRechercher_onclick()
     }
     if(trouve == true)
     {
-        document.getElementById("lblReponse").innerHTML ="Voici le joueur demande " + nomEtu + " ainsi que son pointage : " + tabPoint[i];
+        document.getElementById("lblReponse").innerHTML ="Voici le joueur demandé est : " + nomEtu + " ainsi que son pointage : " + tabPoint[i];
     }
     else
     {
-        document.getElementById("lblReponse").innerHTML = "Vous navez pas trouver le nom demande";
+        document.getElementById("lblReponse").innerHTML = "Vous n'avez pas trouver le nom demandé";
     }
-}
-function calculRecherche()
-{
-
+    return nomEtu;
 }
